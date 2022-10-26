c9d43cc3ecbf8108e38b6c929a83952c2464a9d2
Lena <lm805146@gmail.com>
2022-10-26 13:16:25 +0300

    first homework

--- part1/index.js
+++ part1/index.js
@@ -1,10 +1,10 @@
 // ********************* 2. N U M B E R S *********************
 // a
-const PI = Math.PI.toFixed(2)
+const PI = +Math.PI.toFixed(2)
 console.log(PI)
 // b
 console.log(0.6+0.7)
-console.log((0.6+0.7).toFixed(1))
+console.log(+(0.6+0.7).toFixed(1))
 // c
 console.log(parseInt("100$"))
 
@@ -14,10 +14,15 @@ const str = "some test string"
 console.log(str[0])
 console.log(str[str.length-1])
 // b
-let newStr = str[0].toUpperCase() + str.slice(1, str.length - 2) + str[str.length-1].toUpperCase()
+
+let firtsUpperCase = str.charAt(0).toUpperCase() + str.slice(1)
+let lastUpperCase = firtsUpperCase.split('')
+lastUpperCase[lastUpperCase.length - 1] = firtsUpperCase.charAt(firtsUpperCase.length - 1).toUpperCase()
+let newStr = lastUpperCase.join('')
 console.log(newStr)
 // c
-console.log(str.lastIndexOf(' ')+1)
+let position = str.indexOf(' ', str.indexOf(' ') + 1)
+console.log(position)
 // d
 console.log(str.slice(0, str.length-6))
 
@@ -36,5 +41,18 @@ function entrance(floatNumber) {
         console.log("There is no float with this number.")
     }
 }
+
+function entrance1(floatNumber) {
+    if(floatNumber < 1 && floatNumber > 60)
+        return 'There is no float with this number.'
+    if (floatNumber >= 1 && floatNumber <= 20)
+        return 'Number of the entrance 1.'
+    else if (floatNumber > 20 && floatNumber <= 40)
+        return 'Number of the entrance 2.'
+    else 
+        return 'Number of the entrance 3.'
+}
+
+console.log(entrance1(125))
 
 
