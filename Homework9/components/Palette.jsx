f7245807d262f08638930fda6b9b81c34a511743
Lena <lm805146@gmail.com>
2022-12-25 00:04:30 +0200

    homework9

---
+++ Homework9/components/Palette.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class Palette extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className='wrapper'>
+                Hello World!
+            </div>
+        )
+    }
+}
